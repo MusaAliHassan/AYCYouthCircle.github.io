46ae9f4e3b4362ea09dca0d0bd0406334dc34013
MusaAliHassan <103050479+MusaAliHassan@users.noreply.github.com>
2023-12-27 14:04:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const db = firebase.firestore();
+
     document.getElementById('birthdate').addEventListener('input', checkAge);
 
     function checkAge() {
@@ -30,4 +32,31 @@ document.addEventListener('DOMContentLoaded', function () {
             field.style.display = 'none';
         });
     }
+
+    document.getElementById('signupButton').addEventListener('click', function () {
+        const fullName = document.getElementById('fullName').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const birthdate = document.getElementById('birthdate').value;
+        const password = document.getElementById('password').value;
+        const guardianName = document.getElementById('guardianName').value;
+        const guardianEmail = document.getElementById('guardianEmail').value;
+        const guardianPhone = document.getElementById('guardianPhone').value;
+
+        db.collection('users').add({
+            fullName: fullName,
+            email: email,
+            phone: phone,
+            birthdate: birthdate,
+            guardianName: guardianName,
+            guardianEmail: guardianEmail,
+            guardianPhone: guardianPhone
+        })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+    });
 });
